aea1f9dfd03bab3c7a666b0038b69d0fac700bf0
vadzola <wadzola@yahoo.com.hk>
2022-02-18 15:21:31 +0100

    publish

---
+++ build/precache-manifest.6fb4c53779177dfcdcd7ff706ba3034f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6506c7ec7c9977aa510d33f1780c4fd8",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "c598270d9fa2ce101421",
+    "url": "/monsters-rolodex-complete/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c598270d9fa2ce101421",
+    "url": "/monsters-rolodex-complete/static/js/main.9dc23619.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
